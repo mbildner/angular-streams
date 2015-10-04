3fbf13d7a71a31dd30674013004f3c2d4c03f48f
Moshe Bildner <moshe.bildner@gmail.com>
2015-10-04 17:54:06 -0400

    working map each and merge

--- spec/demoSpec.js
+++ spec/demoSpec.js
@@ -1,7 +1,148 @@
-describe('demo', function(){
-  it('works', function(){
-    var a  = angular.noop();
-    expect(a).toBe(undefined);
-    expect(typeof angular.forEach).toBe('function');
+'use strict';
+
+describe('angular streaming service', function(){
+  var $q;
+  var $rootScope;
+  var StreamFactory;
+
+  beforeEach(function(){
+    module('mhb.stream');
   });
+
+  beforeEach(inject(function(_$q_, _StreamFactory_, _$rootScope_){
+    $q = _$q_;
+    $rootScope = _$rootScope_;
+
+    StreamFactory = _StreamFactory_;
+  }));
+
+  it('wraps a promise', function(){
+    var deferred = $q.defer();
+    var stream = StreamFactory(deferred.promise);
+
+    var holder = [];
+    stream.each(function(event){
+      holder.push(event);
+    });
+
+    deferred.notify('first');
+    deferred.notify('second');
+    deferred.notify('third');
+
+    $rootScope.$digest();
+    expect(holder).toEqual(['first', 'second', 'third']);
+  });
+
+  describe('#map', function(){
+    it('does not mutate the original stream', function(){
+      var deferred = $q.defer();
+      var stream = StreamFactory(deferred.promise);
+
+      var streamOfLols = stream.map(function(event){
+        return 'lol';
+      });
+
+      var lolHolder = [];
+      streamOfLols.each(function(lol){
+        lolHolder.push(lol);
+      });
+
+      var holder = [];
+      stream.each(function(event){
+        holder.push(event);
+      });
+
+      deferred.notify('first');
+      deferred.notify('second');
+      deferred.notify('third');
+
+      $rootScope.$digest();
+      expect(holder).toEqual(['first', 'second', 'third']);
+      expect(lolHolder).toEqual(['lol', 'lol', 'lol']);
+    });
+
+    it('transforms values before sending them through', function(){
+      var deferred = $q.defer();
+      var stream = StreamFactory(deferred.promise);
+
+      var holder = [];
+      stream
+        .map(function(event){
+          return event + ' was mapped';
+        })
+        .each(function(event){
+          holder.push(event);
+        });
+
+      deferred.notify('first');
+      deferred.notify('second');
+      deferred.notify('third');
+
+      $rootScope.$digest();
+      expect(holder).toEqual(['first was mapped', 'second was mapped', 'third was mapped']);
+    });
+  });
+
+  describe('#each', function(){
+    it('returns the original stream', function(){
+      var deferred = $q.defer();
+      var stream = StreamFactory(deferred.promise);
+
+      var holder = [];
+      stream
+        .each(function(event){
+          holder.push(event + ' 1st');
+        })
+        .each(function(event){
+          holder.push(event + ' 2nd');
+        });
+
+      deferred.notify('first');
+      deferred.notify('second');
+      deferred.notify('third');
+
+      $rootScope.$digest();
+      expect(holder).toEqual([
+        'first 1st',
+        'first 2nd',
+        'second 1st',
+        'second 2nd',
+        'third 1st',
+        'third 2nd'
+      ]);
+    });
+  });
+
+  describe('#merge', function(){
+    it('provides events from each stream to one interface', function(){
+      var deferred1 = $q.defer();
+      var deferred2 = $q.defer();
+
+      var stream1 = StreamFactory(deferred1.promise);
+      var stream2 = StreamFactory(deferred2.promise);
+
+      var both = stream1.merge(stream2);
+
+      var holder = [];
+      both.each(function(event){
+        holder.push(event);
+      });
+
+      deferred1.notify('d1 notified 1');
+      deferred1.notify('d2 notified 1');
+
+      deferred1.notify('d1 notified 2');
+      deferred1.notify('d2 notified 2');
+
+      $rootScope.$digest();
+
+      expect(holder).toEqual([
+        'd1 notified 1',
+        'd2 notified 1',
+        'd1 notified 2',
+        'd2 notified 2'
+      ]);
+    });
+  });
+
 });
---
+++ src/app.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module('mhb.stream', []);
+
+angular.module('mhb.stream')
+.factory('StreamFactory', function($q){
+  function Stream(promise, mapFunc){
+    var self = this;
+    var callbackQueue = [];
+    mapFunc = (mapFunc || angular.identity);
+
+    promise.then(null, null, processCallbackQueue);
+
+    function processCallbackQueue(event){
+      angular.forEach(callbackQueue, function(cb){
+        cb(mapFunc(event));
+      });
+    }
+
+    this.each = function(cb){
+      callbackQueue.push(cb);
+      return self;
+    };
+
+    this.map = function(transformCallback){
+      var mappedStream = new Stream(promise, transformCallback);
+      return mappedStream;
+    };
+
+    this.merge = function(target){
+      var resultDeferred = $q.defer();
+
+      var resultStream = new Stream(resultDeferred.promise);
+
+      self.each(resultDeferred.notify);
+      target.each(resultDeferred.notify);
+
+      return resultStream;
+
+    };
+  }
+  return function (promise) { return new Stream(promise); };
+
+});
